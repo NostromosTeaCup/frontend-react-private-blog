671760f8f47f7d2209a3514face887e0a9d5bfd3
NostromosTeaCup <lvanmierlo@protonmail.com>
2022-10-07 10:43:28 +0200

    made directories for pages and imported into app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Overview from './pages/Overview/Overview';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import BlogPost from './pages/BlogPost/BlogPost'
+
 
 function App() {
   // We houden in de state bij of iemand is "ingelogd" (simpele versie)
